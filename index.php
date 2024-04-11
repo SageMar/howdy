ce93bd9446df3ad93472b2b59ef9e497a691cdb3
Sage Markwardt <sagemarkwardt@gmail.com>
2024-04-11 10:24:10 -0400

    created controller file / index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+// 328/howdy/index.php
+// this file holds the controller for Fat-Free
+
+// turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// require the autoload file
+require_once('vendor/autoload.php');
+
+// instantiate the F3 framework Base class
+$f3 = Base::instance();
+
+// define a default route
+$f3->route('GET /', function () {
+    // just echo howdy as our default for now
+    echo '<h1>Howdy!</h1>';
+});
+
+// Run fat-free
+$f3->run();
+
+?>
